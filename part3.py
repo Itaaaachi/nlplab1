ddedfa2024ca1e62df91559dce47c5801baca7a1
Rooooooonaldo <47139783+Itaaaachi@users.noreply.github.com>
2020-11-25 18:54:23 +0800

    评价和trie正向，未debug

--- part3.py
+++ part3.py
@@ -1,11 +1,107 @@
-import time
-
 #输入路径
 FMM_Path = 'text/seg_FMM.txt'
 BMM_Path = 'text/seg_BMM.txt'
-Standard_path = 'text/199801_seg&pos.txt'
+Original_path = 'text/199801_seg&pos.txt'
 
 #输出路径
-Result_Path = 'text/score.txt'
+Score_Path = 'text/score.txt'
+
+def unified(para):
+    if para == 1:
+        path = 'text/199801_seg&pos.txt'
+        encoding = 'gbk'
+    elif para == 2:
+        path = 'text/seg_FMM.txt'
+        encoding = 'utf-8'
+    elif para == 3:
+        path = 'text/seg_BMM.txt'
+        encoding = 'utf-8'
+    readfile = open(path, 'r', encoding=encoding)
+    result = []
+    for line in readfile:
+        if line == '\n':  # 去掉空行
+            continue
+        new = ''
+        for letter in line.split():
+            if letter[0] == '[':
+                new += letter[1:letter.index('/')] + '/ '
+            else:
+                new += letter[0:letter.index('/')] + '/ '
+        result.append(new)
+    return result
 
 
+def score(para):
+    standard, right, mm, index, i, j = 0, 0, 0, 0, 0, 0
+    standard_lines = unified(1)
+    mm_lines = unified(para)
+
+    for line in standard_lines:
+        standard_words = line.split('/ ')
+        mm_words = mm_lines[index].split('/ ')
+        index += 1
+        standard_size = len(standard_words) - 1  # 去掉每行句尾的空格
+        mm_size = len(mm_words) - 1
+        standard += standard_size
+        mm += mm_size
+        standard_length = len(standard_words[0])
+        mm_length = len(mm_words[0])
+        while (i < standard_size) and (j < mm_size):
+
+            if standard_length == mm_length:
+                right += 1
+                if i == standard_size - 1:
+                    break
+                i += 1
+                j += 1
+                standard_length += len(standard_words[i])
+                mm_length += len(mm_words[j])
+
+            else:
+                while True:
+                    if standard_length < mm_length:
+                        i += 1
+                        standard_length += len(standard_words[i])
+
+                    elif standard_length > mm_length:
+                        j += 1
+                        mm_length += len(mm_words[j])
+                    else:
+                        if i < standard_size - 1:
+                            standard_length += len(standard_words[i + 1])
+                            mm_length += len(mm_words[j + 1])
+                        i += 1
+                        j += 1
+                        break
+        i = j = 0
+    p = right / standard
+    r = right / mm
+    f = p * r * 2 / (p + r)
+    print("标准词库词数：" + str(standard))
+    if para == 2:
+        print("FMM结果为：")
+        print("FMM词数：" + str(mm))
+    elif para == 3:
+        print("BMM结果为：")
+        print("BMM词数：" + str(mm))
+    print("正确词数：" + str(right))
+    print("准确率：" + str(p * 100) + "%")
+    print("召回率：" + str(r * 100) + "%")
+    print("F值：" + str(f * 100) + "%")
+    writefile = open(Score_Path, 'a+', encoding='UTF-8')
+
+    if para == 2:
+        writefile.write("FMM结果为：\n")
+        writefile.write("FMM词数：" + str(mm) + "\n")
+    elif para == 3:
+        writefile.write("BMM结果为：\n")
+        writefile.write("BMM词数：" + str(mm) + "\n")
+    writefile.write("标准词库词数：" + str(standard) + "\n")
+    writefile.write("正确词数：" + str(right) + "\n")
+    writefile.write("准确率：" + str(p * 100) + "%\n")
+    writefile.write("召回率：" + str(r * 100) + "%\n")
+    writefile.write("F值：" + str(f * 100) + "%\n")
+
+
+score(2)
+score(3)
--- part4.py
+++ part4.py
@@ -1,31 +1,146 @@
 # 输入路径
-FilePath = 'text/199801_sent.txt'
+File_Path = 'text/199801_sent.txt'
+Dic_Path = 'text/dic.txt'
 
 # 输出路径
-ResultPath = 'text/TimeCost.txt'
+Result_Path = 'text/TimeCost.txt'
+FMM_Path_2='text/Seg_FMM_2.txt'
 
+#Trie节点
+class TrieNode:
+    # 构造函数
+    def __init__(self, char):
+        self.char = char
+        self.childlist = []
 
-class Trie:
-    #构造函数
-    def _init_(self, character, terminal, children):
-        self.character = character
-        self.terminal = terminal
-        self.children = children
+    def getchar(self):
+        return '%s' % self.char
+
+    def getList(self):
+        return self.childlist
+
+    def inList(self, char):
+        List = self.getList()
+        for i in range(len(List)):
+            if(List[i].getchar == char):
+                return True
+        return False
+
+# 向Trie中插入新词中剩余部分
+def insert(current, rest):
+    p = current.getList()
+    if rest == "":
+        # 词尾
+        if not current.inList('#'):
+            p.append('#')
+            return
+        # Trie中已有
+        else:
+            return
+    # 遍历当前节点所有子节点
+    for node in p:
+        if p.getchar == rest[0]:
+            insert(node, rest[1:])
+            return
+    new = TrieNode(rest[0])
+    current.getList().append(new)
+    insert(new, rest[1:])
+    return
+
+#向Trie中插入新词
+def wordInsert(root,word):
+    current = root
+    for w in range(len(word)):
+        l = current.getList()
+        for p in l:
+            if word[w] == p.getchar():
+                current = p
+                break
+    insert(current,word[w:])
+
+# 从Trie中搜索
+def search(word, root):
+    # 初始化当前节点
+    current = root
+    # 遍历词中每个字
+    for w in word:
+        l = current.getList()
+        # 遍历当前节点的所有儿子
+        for p in l:
+            if w == p.getchar():
+                current = p
+                break
+        return False
+    # 检查当前节点的儿子节点中是否有'#'，如果有，则有以此字结尾的词，否则没有
+    for k in current.getList():
+        if k.getchar() == '#':
+            return True
+    return False
+
+# 构建正向最大匹配Trie
+def set_fmm_trie():
+    file = open(Dic_Path, 'r', encoding='utf-8')
+    try:
+        b = file.read()  # 读取词典
+    finally:
+        file.close()
+    # 将词典转化为list
+    dic = b.split('\n')
+    trie=TrieNode("")
+    for word in dic:
+        wordInsert(trie,word)
+    return trie
+
+#从Trie中搜索
+def search_fmm_trie(trie):
+    MaxLen=11
+    segList = []
+    readfile = open(File_Path, 'r', encoding='gbk')
+    try:
+        lines = readfile.readlines()  # 读取Sent.txt
+    finally:
+        readfile.close()
+    writefile = open(FMM_Path_2, 'w', encoding='utf-8')
+    try:
+        for line in lines:
+            line = line[:len(line) - 1]  # 去掉每行最后的\n
+            while len(line) > 0:
+                if len(line) < MaxLen:
+                    tryWord = line[0:len(line)]
+                else:
+                    tryWord = line[0:MaxLen]
+                p=search(tryWord,trie)
+                while not p:
+                    # 若字串长度为1，跳出循环
+                    if len(tryWord) == 1:
+                        break
+                    # 截掉子串尾部一个字，用剩余部分到字典中匹配
+                    tryWord = tryWord[0:len(tryWord) - 1]
+                # 将匹配成功的词加入到分词列表中
+                segList.append(tryWord + '/ ')
+                # 将匹配成功的词从待分词字符串中去除，继续循环，直到分词完成
+                line = line[len(tryWord):]
+            segList.append('\n')
+        writefile.write(pre_line(''.join(segList)))
+    finally:
+        writefile.close()
     
-    #返回当前节点的terminal
-    def in_terminal(self):
-        return self.terminal
-    
-    #设置当前节点terminal
-    def set_terminal(self,terminal):
-        self.terminal=terminal
+def pre_line(line):
+    punctuation = '-./'
+    buffer, result = '', ''
+    word_list = line.split('/ ')
+    word_list = word_list[:len(word_list) - 1]
+    for idx, word in enumerate(word_list):
+        if word.isascii() or word in punctuation:  # 若是字母、数字或者英文标点
+            buffer += word
+            if idx + 1 == len(word_list):
+                result += buffer + '/ '
+        else:
+            if buffer:
+                result += buffer + '/ '
+                buffer = ''
+            result += word + '/ '
+    return result
 
-    #返回当前节点字符
-    def get_character(self):
-        return self.character
-
-    #设置当前节点terminal
-    def set_character(self,character):
-        self.character=character
-
-    
+trie=set_fmm_trie()
+search_fmm_trie(trie)
